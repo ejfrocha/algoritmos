cd6458f819a94fe437ab4eedfd22a89b3514bc2b
ejfrocha <102183116+ejfrocha@users.noreply.github.com>
2022-03-30 00:56:46 -0300

    Update SortComparison.py

--- SortComparison.py
+++ SortComparison.py
@@ -1,42 +1,46 @@
-#Esta versão utiliza os dois algoritmos e compara seus tempos dentro de um arquivo de saída
-
 # Import OS Module for opening files
 import os
 # Import Time Module for measuring performance
 import time
 
-# Folder with cases
-path = "C:\Dev\Python\casos"
-# Change the directory
-os.chdir(path)
+# Get all inputs available in the Instancies folder, and run Comparison to the Sort Methods
+def loadSortInputs():
+	for fileName in os.listdir():
+		# Check if file is input (and not output)
+		if fileName.endswith(".in"):
+			print(fileName)
+			#Calls Sort Comparison Function
+			compareSorts(fileName)
 
-def openCases():
-
-	inputCases = open("num.100000.1.in", "r")
-	outputCases = open("outputs.txt", "a")
+# Function to execute Insert and Selection sorts, and compare results
+def compareSorts(inputFile):
+	
+	inputCases = open(inputFile, "r")
+	outputCases = open(outputFile, "a")
 	
 	totalCases = int(inputCases.readline())
+	print(totalCases)
 	inputArray = inputCases.read()
 	
 	#Making text array as Integers
 	integerInputArray = [int(i) for i in inputArray.split()]
+	#Making a hard copy of the above Array, so the other funcion can be compared with the same inputs
 	integerInputArray2 = integerInputArray.copy()
 	
-	ticInsertion = time.perf_counter()
-	insertionSort(integerInputArray, totalCases)
-	tacInsertion = time.perf_counter()
-	durationInsertion = (ticInsertion - tacInsertion)
-	
-	print(integerInputArray)
-	
+	#Selection Sort with performance counter
 	ticSelection = time.perf_counter()
-	selectionSort(integerInputArray2, totalCases)
+	selectionSort(integerInputArray, totalCases)
 	tacSelection = time.perf_counter()
 	durationSelection = (ticSelection - tacSelection)
 	
-	print(integerInputArray2)
-	
-	results = 'For #' + str(totalCases) + '\n' + 'Insertion = ' + str(durationInsertion) + ' & ' + 'Selection = ' + str(durationSelection) + ' | Difference of ' + str(durationInsertion - durationSelection)
+	#Insertion Sort with performance counter
+	ticInsertion = time.perf_counter()
+	insertionSort(integerInputArray2, totalCases)
+	tacInsertion = time.perf_counter()
+	durationInsertion = (ticInsertion - tacInsertion)
+
+	#Print results into an output file
+	results = 'For ' + inputFile + ' with #' + str(totalCases) + '\n' + 'Insertion = ' + str(durationInsertion) + ' & ' + 'Selection = ' + str(durationSelection) + ' | Difference of ' + str(durationInsertion - durationSelection)
 	outputCases.write(results)
 	outputCases.write(':\n')
 	
@@ -44,6 +48,7 @@ def openCases():
 	outputCases.close()
 
 
+# Insertion Sort function
 def insertionSort(array, n):
 	for i in range(1, n):
 		current = array[i]
@@ -54,6 +59,7 @@ def insertionSort(array, n):
 		
 		array[j + 1] = current
 
+# Selection Sort function
 def selectionSort(array, n):
     for current in range(n):
         min = current
@@ -61,10 +67,20 @@ def selectionSort(array, n):
             if array[i] < array[min]:
                 min = i
         (array[current], array[min]) = (array[min], array[current])
-	
 
-openCases()
-print('Arrays sorted and time printed in the output file')
+# Setting path and names
+os.chdir("C:\Dev\Python\casos\instancias")
+outputFile = "outputs.txt"
+
+# Renew the Output file
+outputCases = open(outputFile, "w")
+outputCases.close()
+
+#Load Input Cases
+loadSortInputs()
+
+#Conclusion Message
+print('Inputs Sorted and Performance Measured into Output file')
 
 
 
