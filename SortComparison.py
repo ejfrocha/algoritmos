6d8d08059c071fc98eabd32021713031fc9df7e1
ejfrocha <102183116+ejfrocha@users.noreply.github.com>
2022-03-30 00:07:08 -0300

    Update SortComparison.py

--- SortComparison.py
+++ SortComparison.py
@@ -55,19 +55,12 @@ def insertionSort(array, n):
 		array[j + 1] = current
 
 def selectionSort(array, n):
-   
-    for i in range(n):
-        min = i
-
-        for i in range(i + 1, n):
-         
-            # to sort in descending order, change > to < in this line
-            # select the minimum element in each loop
+    for current in range(n):
+        min = current
+        for i in range(current + 1, n):
             if array[i] < array[min]:
                 min = i
-         
-        # put min at the correct position
-        (array[i], array[min]) = (array[min], array[i])
+        (array[current], array[min]) = (array[min], array[current])
 	
 
 openCases()
